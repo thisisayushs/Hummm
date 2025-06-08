6fe93b1634d462db31a1880e22eb09d3126350cc
Ayush Singh <singh.ayush.as@icloud.com>
2025-06-08 20:53:34 +0200

    Initial Commit

---
+++ Hummm Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hummm Watch App
+//
+//  Created by Ayush Singh on 6/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hummm Watch App/HummmApp.swift
@@ -0,0 +1,17 @@
+//
+//  HummmApp.swift
+//  Hummm Watch App
+//
+//  Created by Ayush Singh on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Hummm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
